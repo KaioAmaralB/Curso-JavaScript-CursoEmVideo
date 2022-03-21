429e08174caf0147c4dbe426aba313ff9b094095
KaioBispo <kaio411@hotmail.com>
2022-03-21 16:30:34 -0300

    Subindo o projeto do curso

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60){ // Condição simples
+    console.log("Você ultrapassou a velocidade permitida, MULTADO")
+}
+console.log("Dirija sempre usando cinto de segurança")
